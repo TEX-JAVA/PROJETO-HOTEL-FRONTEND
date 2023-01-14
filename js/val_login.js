3e092c2864c7f8d581eaceccb8039ebfbc2ed742
DaviNunes05 <davi.nunes05@gmail.com>
2023-01-14 10:20:49 -0300

    Incluso interação de login em todas as páginas.

--- js/val_login.js
+++ js/val_login.js
@@ -1,11 +1,18 @@
 const usuario = "Fulaninho";
 const btnLogin = document.getElementById("login");
+const btnSair = document.getElementById("sair");
 const email = document.getElementById("login_email");
+
+var lsNome = localStorage.getItem("nome");
+var lsEmail = localStorage.getItem("email");
+
+if(localStorage.getItem('nome') != null && localStorage.getItem('email') != null){
+	document.getElementById("visitante").innerHTML = `Olá! ${lsNome}`;
+	document.getElementById("menu-sidebar-visitante").innerHTML = lsNome;
+}
 
 btnLogin.onclick = () => {
 	let formEmail = email.value.replace(/'|"|\s/g, "").toLowerCase();
-	let lsNome, lsEmail;
-
 	if (formEmail == "") {
 		alert("Preencha seu E-mail");
 	} else {
@@ -18,3 +25,11 @@ btnLogin.onclick = () => {
 		alert(`Olá ${lsNome}(${lsEmail})`);
 	}
 };
+
+btnSair.onclick = () => {
+	localStorage.clear("nome");
+	localStorage.clear("email");
+	document.getElementById("visitante").innerHTML = `Olá! Visitante`;
+	document.getElementById("menu-sidebar-visitante").innerHTML = "Login";
+	location.reload();
+}
